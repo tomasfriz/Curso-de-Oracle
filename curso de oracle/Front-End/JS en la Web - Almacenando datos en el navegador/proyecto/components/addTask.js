551d304062a949a965871199afa726bb3f13e178
tomasfriz <tomyagustinf@gmail.com>
2022-06-20 16:53:16 -0300

    terminado definitivamente

--- curso de oracle/Front-End/JS en la Web - Almacenando datos en el navegador/proyecto/components/addTask.js
+++ curso de oracle/Front-End/JS en la Web - Almacenando datos en el navegador/proyecto/components/addTask.js
@@ -1,5 +1,6 @@
 import checkComplete from './checkComplete.js';
 import deleteIcon from './deleteIcon.js';
+import { displayTasks } from './readTasks.js';
 
 export const addTask = (evento) => {
   evento.preventDefault();
@@ -12,39 +13,51 @@ export const addTask = (evento) => {
   const date = calendar.value;
   const dateFormat = moment(date).format('DD/MM/YYYY');
 
+  if (value === '' || date === '') {
+    return;
+  }
+
   input.value = '';
   calendar.value = '';
+
+  const complete = false;
 
   const taskObj = {
     value,
     dateFormat,
+    complete,
+    id: uuid.v4(),
   };
+
+  list.innerHTML = '';
 
   const taskList = JSON.parse(localStorage.getItem('tasks')) || [];
   taskList.push(taskObj);
   localStorage.setItem('tasks', JSON.stringify(taskList));
 
-  const task = createTask(taskObj);
-  list.appendChild(task);
+  displayTasks();
 };
 
-export const createTask = ({ value, dateFormat }) => {
+export const createTask = ({ value, dateFormat, complete, id }) => {
   const task = document.createElement('li');
   task.classList.add('card');
 
   const taskContent = document.createElement('div');
 
+  const check = checkComplete(id);
+
+  if (complete) {
+    check.classList.toggle('fas');
+    check.classList.toggle('completeIcon');
+    check.classList.toggle('far');
+  }
   const titleTask = document.createElement('span');
   titleTask.classList.add('task');
   titleTask.innerText = value;
-  taskContent.appendChild(checkComplete());
+  taskContent.appendChild(check);
   taskContent.appendChild(titleTask);
 
-  const dateElement = document.createElement('span');
-  dateElement.innerHTML = dateFormat;
-
   task.appendChild(taskContent);
-  task.appendChild(dateElement);
-  task.appendChild(deleteIcon());
+  task.appendChild(deleteIcon(id));
   return task;
 };
